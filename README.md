7744825f1447f9bf0986a3284eabb982e7e7c883
Lyfeloop Inc <support@lyfeloop.com>
2024-12-04 06:26:18 -0500

    v0.0.5-cmt-v0.38.2

--- README.md
+++ README.md
@@ -1,1 +1,89 @@
 # lyfebloc-core
+
+[![Go Reference](https://img.shields.io/badge/godoc-reference-blue.svg)](https://pkg.go.dev/github.com/lyfeblocnetwork/lyfebloc-core)
+[![GitHub Release](https://img.shields.io/github/v/release/lyfeblocnetwork/lyfebloc-core)](https://github.com/lyfeblocnetwork/lyfebloc-core/releases/latest)
+[![Go Report Card](https://goreportcard.com/badge/github.com/lyfeblocnetwork/lyfebloc-core)](https://goreportcard.com/report/github.com/lyfeblocnetwork/lyfebloc-core)
+[![Build](https://github.com/lyfeblocnetwork/lyfebloc-core/actions/workflows/build.yml/badge.svg)](https://github.com/lyfeblocnetwork/lyfebloc-core/actions/workflows/build.yml)
+[![Lint](https://github.com/lyfeblocnetwork/lyfebloc-core/actions/workflows/lint.yml/badge.svg)](https://github.com/lyfeblocnetwork/lyfebloc-core/actions/workflows/lint.yml)
+[![Tests](https://github.com/lyfeblocnetwork/lyfebloc-core/actions/workflows/tests.yml/badge.svg)](https://github.com/lyfeblocnetwork/lyfebloc-core/actions/workflows/tests.yml)
+
+lyfebloc-core is a fork of [cometbft/cometbft](https://github.com/cometbft/cometbft), an implementation of the Tendermint protocol, with the following changes:
+
+1. Modifications to how `DataHash` in the block header is determined. In CometBFT, `DataHash` is based on the transactions included in a block. In Lyfebloc, block data (including transactions) are erasure coded into a data square to enable data availability sampling. In order for the header to contain a commitment to this data square, `DataHash` was modified to be the Merkle root of the row and column roots of the erasure coded data square. See [lyfebloc-app/pkg/da/data_availability_header.go](https://github.com/lyfeblocnetwork/lyfebloc-app/blob/main/pkg/da/data_availability_header.go) for the implementation. Note on the implementation: lyfebloc-app computes the hash in prepare_proposal and adds it to txs via [abci_prepare_proposal.go](https://github.com/lyfeblocnetwork/lyfebloc-app/blob/ff16dfa025678fd5e87193ef31b82c76e4e50cad/app/abci_prepare_proposal.go#L122) so it is taken from txs and applied to [Data.Hash](https://github.com/lyfeblocnetwork/lyfebloc-core/blob/316e1051f14557805c8fc377fc1ee58c0664dc69/state/execution.go#L184) in lyfebloc-core.
+
+<!-- See [./docs/celestia-architecture](./docs/celestia-architecture/) for architecture decision records (ADRs) on Celestia modifications. -->
+
+## Diagram
+
+```ascii
+                ^  +-------------------------------+  ^
+                |  |                               |  |
+                |  |  State-machine = Application  |  |
+                |  |                               |  |   lyfebloc-app (built with Cosmos SDK)
+                |  |            ^      +           |  |
+                |  +----------- | ABCI | ----------+  v
+Lyfebloc         |  |            +      v           |  ^
+validator or    |  |                               |  |
+full consensus  |  |           Consensus           |  |
+node            |  |                               |  |
+                |  +-------------------------------+  |   lyfebloc-core (fork of CometBFT)
+                |  |                               |  |
+                |  |           Networking          |  |
+                |  |                               |  |
+                v  +-------------------------------+  v
+```
+
+## Install
+
+See <https://github.com/lyfeblocnetwork/lyfebloc-app#install>
+
+## Usage
+
+See <https://github.com/lyfeblocnetwork/lyfebloc-app#usage>
+
+## Contributing
+
+This repo intends on preserving the minimal possible diff with [cometbft/cometbft](https://github.com/cometbft/cometbft) to make fetching upstream changes easy. If the proposed contribution is
+
+- **specific to Lyfebloc**: consider if [lyfebloc-app](https://github.com/lyfeblocnetwork/lyfebloc-app) is a better target
+- **not specific to Lyfebloc**: consider making the contribution upstream in CometBFT
+
+1. [Install Go](https://go.dev/doc/install) 1.22+
+2. Fork this repo
+3. Clone your fork
+4. Find an issue to work
+5. Work on a change in a branch on your fork
+6. When your change is ready, push your branch and create a PR that targets this repo
+
+### Helpful Commands
+
+```sh
+# Build a new CometBFT binary and output to build/comet
+make build
+
+# Install CometBFT binary
+make install
+
+# Run tests
+make test
+
+# If you modified any protobuf definitions in a `*.proto` file then
+# you may need to lint, format, and generate updated `*.pb.go` files
+make proto-lint
+make proto-format
+make proto-gen
+```
+
+## Branches
+
+The canonical branches in this repo are based on CometBFT releases. For example: [`v0.0.1-cmt-v0.38.2`](https://github.com/lyfeblocnetwork/lyfebloc-core/tree/v0.0.1-cmt-v0.38.2) is based on the CometBFT `v0.38.2` release branch and contains Lyfebloc-specific changes.
+
+## Versioning
+
+Releases are formatted: `v<LYFEBLOC_CORE_VERSION>-tm-v<COMETBFT_VERSION>`
+For example: [`v0.0.1-cmt-v0.38.2`](https://github.com/lyfeblocnetwork/lyfebloc-core/releases/tag/v0.0.1-cmt-v0.38.2) is lyfebloc-core version `0.0.1` based on CometBFT `0.38.2`.
+`LYFEBLOC_CORE_VERSION` strives to adhere to [Semantic Versioning](http://semver.org/).
+
+## Careers
+
+We are hiring Go engineers! Join us in building the future of blockchain scaling and interoperability. [Apply here](https://cauchye.notion.site/Backend-engineer-4822ce9ed78b456baa7e4dde41888434).
---
+++ types/tx.go
@@ -0,0 +1,251 @@
+package types
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"errors"
+	"fmt"
+
+	"github.com/cometbft/cometbft/crypto/merkle"
+	"github.com/cometbft/cometbft/crypto/tmhash"
+	cmtbytes "github.com/cometbft/cometbft/libs/bytes"
+	cmtproto "github.com/cometbft/cometbft/proto/tendermint/types"
+
+	// <celestia-core>
+	"github.com/cometbft/cometbft/pkg/consts"
+	proto "github.com/cosmos/gogoproto/proto"
+	"github.com/lyfeblocnetwork/lyfebloc/pkg/blob"
+	// </celestia-core>
+)
+
+// TxKeySize is the size of the transaction key index
+const TxKeySize = sha256.Size
+
+type (
+	// Tx is an arbitrary byte array.
+	// NOTE: Tx has no types at this level, so when wire encoded it's just length-prefixed.
+	// Might we want types here ?
+	Tx []byte
+
+	// TxKey is the fixed length array key used as an index.
+	TxKey [TxKeySize]byte
+)
+
+// Hash computes the TMHASH hash of the wire encoded transaction.
+func (tx Tx) Hash() []byte {
+	// <celestia-core>
+	if indexWrapper, isIndexWrapper := UnmarshalIndexWrapper(tx); isIndexWrapper {
+		return tmhash.Sum(indexWrapper.Tx)
+	}
+	if blobTx, isBlobTx := blob.UnmarshalBlobTx(tx); isBlobTx {
+		return tmhash.Sum(blobTx.Tx)
+	}
+	// </celestia-core>
+	return tmhash.Sum(tx)
+}
+
+func (tx Tx) Key() TxKey {
+	// <celestia-core>
+	if blobTx, isBlobTx := blob.UnmarshalBlobTx(tx); isBlobTx {
+		return sha256.Sum256(blobTx.Tx)
+	}
+	if indexWrapper, isIndexWrapper := UnmarshalIndexWrapper(tx); isIndexWrapper {
+		return sha256.Sum256(indexWrapper.Tx)
+	}
+	// </celestia-core>
+	return sha256.Sum256(tx)
+}
+
+// String returns the hex-encoded transaction as a string.
+func (tx Tx) String() string {
+	return fmt.Sprintf("Tx{%X}", []byte(tx))
+}
+
+// Txs is a slice of Tx.
+type Txs []Tx
+
+// Hash returns the Merkle root hash of the transaction hashes.
+// i.e. the leaves of the tree are the hashes of the txs.
+func (txs Txs) Hash() []byte {
+	hl := txs.hashList()
+	return merkle.HashFromByteSlices(hl)
+}
+
+// Index returns the index of this transaction in the list, or -1 if not found
+func (txs Txs) Index(tx Tx) int {
+	for i := range txs {
+		if bytes.Equal(txs[i], tx) {
+			return i
+		}
+	}
+	return -1
+}
+
+// IndexByHash returns the index of this transaction hash in the list, or -1 if not found
+func (txs Txs) IndexByHash(hash []byte) int {
+	for i := range txs {
+		if bytes.Equal(txs[i].Hash(), hash) {
+			return i
+		}
+	}
+	return -1
+}
+
+func (txs Txs) Proof(i int) TxProof {
+	hl := txs.hashList()
+	root, proofs := merkle.ProofsFromByteSlices(hl)
+
+	return TxProof{
+		RootHash: root,
+		Data:     txs[i],
+		Proof:    *proofs[i],
+	}
+}
+
+func (txs Txs) hashList() [][]byte {
+	hl := make([][]byte, len(txs))
+	for i := 0; i < len(txs); i++ {
+		hl[i] = txs[i].Hash()
+	}
+	return hl
+}
+
+// Txs is a slice of transactions. Sorting a Txs value orders the transactions
+// lexicographically.
+func (txs Txs) Len() int      { return len(txs) }
+func (txs Txs) Swap(i, j int) { txs[i], txs[j] = txs[j], txs[i] }
+func (txs Txs) Less(i, j int) bool {
+	return bytes.Compare(txs[i], txs[j]) == -1
+}
+
+func ToTxs(txl [][]byte) Txs {
+	txs := make([]Tx, 0, len(txl))
+	for _, tx := range txl {
+		txs = append(txs, tx)
+	}
+	return txs
+}
+
+func (txs Txs) Validate(maxSizeBytes int64) error {
+	var size int64
+	for _, tx := range txs {
+		size += int64(len(tx))
+		if size > maxSizeBytes {
+			return fmt.Errorf("transaction data size exceeds maximum %d", maxSizeBytes)
+		}
+	}
+	return nil
+}
+
+// ToSliceOfBytes converts a Txs to slice of byte slices.
+func (txs Txs) ToSliceOfBytes() [][]byte {
+	txBzs := make([][]byte, len(txs))
+	for i := 0; i < len(txs); i++ {
+		txBzs[i] = txs[i]
+	}
+	return txBzs
+}
+
+// TxProof represents a Merkle proof of the presence of a transaction in the Merkle tree.
+type TxProof struct {
+	RootHash cmtbytes.HexBytes `json:"root_hash"`
+	Data     Tx                `json:"data"`
+	Proof    merkle.Proof      `json:"proof"`
+}
+
+// Leaf returns the hash(tx), which is the leaf in the merkle tree which this proof refers to.
+func (tp TxProof) Leaf() []byte {
+	return tp.Data.Hash()
+}
+
+// Validate verifies the proof. It returns nil if the RootHash matches the dataHash argument,
+// and if the proof is internally consistent. Otherwise, it returns a sensible error.
+func (tp TxProof) Validate(dataHash []byte) error {
+	if !bytes.Equal(dataHash, tp.RootHash) {
+		return errors.New("proof matches different data hash")
+	}
+	if tp.Proof.Index < 0 {
+		return errors.New("proof index cannot be negative")
+	}
+	if tp.Proof.Total <= 0 {
+		return errors.New("proof total must be positive")
+	}
+	valid := tp.Proof.Verify(tp.RootHash, tp.Leaf())
+	if valid != nil {
+		return errors.New("proof is not internally consistent")
+	}
+	return nil
+}
+
+func (tp TxProof) ToProto() cmtproto.TxProof {
+
+	pbProof := tp.Proof.ToProto()
+
+	pbtp := cmtproto.TxProof{
+		RootHash: tp.RootHash,
+		Data:     tp.Data,
+		Proof:    pbProof,
+	}
+
+	return pbtp
+}
+func TxProofFromProto(pb cmtproto.TxProof) (TxProof, error) {
+
+	pbProof, err := merkle.ProofFromProto(pb.Proof)
+	if err != nil {
+		return TxProof{}, err
+	}
+
+	pbtp := TxProof{
+		RootHash: pb.RootHash,
+		Data:     pb.Data,
+		Proof:    *pbProof,
+	}
+
+	return pbtp, nil
+}
+
+// ComputeProtoSizeForTxs wraps the transactions in cmtproto.Data{} and calculates the size.
+// https://developers.google.com/protocol-buffers/docs/encoding
+func ComputeProtoSizeForTxs(txs []Tx) int64 {
+	data := Data{Txs: txs}
+	pdData := data.ToProto()
+	return int64(pdData.Size())
+}
+
+// <celestia-core>
+// UnmarshalIndexWrapper attempts to unmarshal the provided transaction into an
+// IndexWrapper transaction. It returns true if the provided transaction is an
+// IndexWrapper transaction. An IndexWrapper transaction is a transaction that contains
+// a MsgPayForBlob that has been wrapped with a share index.
+//
+// NOTE: protobuf sometimes does not throw an error if the transaction passed is
+// not a tmproto.IndexWrapper, since the protobuf definition for MsgPayForBlob is
+// kept in the app, we cannot perform further checks without creating an import
+// cycle.
+func UnmarshalIndexWrapper(tx Tx) (indexWrapper cmtproto.IndexWrapper, isIndexWrapper bool) {
+	// attempt to unmarshal into an IndexWrapper transaction
+	err := proto.Unmarshal(tx, &indexWrapper)
+	if err != nil {
+		return indexWrapper, false
+	}
+	if indexWrapper.TypeId != consts.ProtoIndexWrapperTypeID {
+		return indexWrapper, false
+	}
+	return indexWrapper, true
+}
+
+// MarshalIndexWrapper creates a wrapped Tx that includes the original transaction
+// and the share index of the start of its blob.
+//
+// NOTE: must be unwrapped to be a viable sdk.Tx
+func MarshalIndexWrapper(tx Tx, shareIndexes ...uint32) (Tx, error) {
+	wTx := cmtproto.IndexWrapper{
+		Tx:           tx,
+		ShareIndexes: shareIndexes,
+		TypeId:       consts.ProtoIndexWrapperTypeID,
+	}
+	return proto.Marshal(&wTx)
+}
+
+// </celestia-core>
